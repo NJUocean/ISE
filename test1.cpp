b9d2c3e4082977e9b18e5ca9c82b8fd2c06820cc
wangyang <cnwy1996@outlook.com>
2019-11-16 09:34:47 +0800

    testcpp

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int f(int x) {
+  int result = (x / 42);
+  int result2 = (x / 42);
+  x++;
+  //sjhidn
+  x=x+1;
+
+  x+=      1;
+  return result;
+}
---
+++ test2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int h(int x) {
+  int result = x / 42;
+  int res = (x / 42);
+  //sjhidn
+  x=x+2;
+
+  x+=      1;
+  return result;
+}
